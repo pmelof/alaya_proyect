83e0e0050d1aa516b68d4b546052f1851854a3ce
Cristobal Medrano Alvarado <cristobal.medrano@usach.cl>
2018-09-09 20:52:00 -0300

    Git ignore actualizado

--- src/main/java/LatteGroup/AlayaIDEA/controller/UsersController.java
+++ src/main/java/LatteGroup/AlayaIDEA/controller/UsersController.java
@@ -2,10 +2,6 @@ package LatteGroup.AlayaIDEA.controller;
 
 import LatteGroup.AlayaIDEA.document.Users;
 import LatteGroup.AlayaIDEA.repository.UsersRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
