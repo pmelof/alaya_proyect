469fe018dd6ba2d3b9759db969c88c5612ee33f8
paty <patricia.melo@usach.cl>
2018-09-10 19:58:15 -0300

    CRUD comentario listo y más

--- src/main/java/LatteGroup/AlayaIDEA/controller/ComentariosController.java
+++ src/main/java/LatteGroup/AlayaIDEA/controller/ComentariosController.java
@@ -16,15 +16,43 @@ public class ComentariosController {
     @Autowired
     private ComentariosRepository comentariosRepository;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/all")
     public List<Comentarios> getAll()
     {
         return comentariosRepository.findAll();
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "/create", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public void create(@RequestBody Comentarios comentarios)
     {
         comentariosRepository.save(comentarios);
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @RequestMapping(value = "/comentario/{id}", method = RequestMethod.GET)    //id del comentario
+    public Comentarios comentario(@PathVariable ("id") Integer id)
+    {
+        return comentariosRepository.findOne(id);
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @RequestMapping(value = "/update/comentario/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Comentarios updateComentario(@PathVariable ("id") Integer id, @RequestBody Comentarios comentario)
+    {
+        Comentarios newComentario = comentariosRepository.findOne(id);
+        newComentario.setTexto(comentario.getTexto());
+        newComentario.setAutor(comentario.getAutor());
+
+        return comentariosRepository.save(newComentario);
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @RequestMapping(value = "/delete/comentario/{id}", method = RequestMethod.DELETE)
+    public void deleteReto(@PathVariable ("id") Integer id)
+    {
+        Comentarios comentario = comentariosRepository.findOne(id);
+        comentariosRepository.delete(comentario);
     }
 }
--- src/main/java/LatteGroup/AlayaIDEA/controller/RestosController.java
+++ src/main/java/LatteGroup/AlayaIDEA/controller/RestosController.java
@@ -18,18 +18,21 @@ public class RestosController {
     @Autowired
     private RetosRepository retosRepository;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/all")
     public List<Retos>  getAll()
     {
         return retosRepository.findAll();
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "/create", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public void create(@RequestBody Retos retos)
     {
         retosRepository.save(retos);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "/add/idea", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, params = {"id"})
     public Retos addIdea(@RequestParam ("id") Integer id, @RequestBody Ideas idea )
     {
@@ -41,13 +44,14 @@ public class RestosController {
         return  retosRepository.save(reto);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "/reto/{id}", method = RequestMethod.GET)    //id de reto
     public Retos reto(@PathVariable ("id") Integer id)
     {
         return retosRepository.findOne(id);
     }
 
-
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "/update/reto/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
     public Retos updateReto(@PathVariable ("id") Integer id, @RequestBody Retos reto)
     {
