d9fe721c33811abb96b8b31bb5668240ffd0ccd0
Cristobal Medrano Alvarado <cristobal.medrano@usach.cl>
2018-09-10 23:18:55 -0300

    CRUD Reto terminado

--- src/main/java/LatteGroup/AlayaIDEA/repository/RetoRepository.java
+++ src/main/java/LatteGroup/AlayaIDEA/repository/RetoRepository.java
@@ -3,5 +3,8 @@ package LatteGroup.AlayaIDEA.repository;
 import LatteGroup.AlayaIDEA.document.Reto;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface RetoRepository extends MongoRepository<Reto, Integer>{
+public interface RetoRepository extends MongoRepository<Reto, Integer>
+{
+    public Reto findRetoById(String id);
+    public Reto findRetoByTitulo(String id);
 }
